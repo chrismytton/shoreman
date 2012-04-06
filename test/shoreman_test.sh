18d3d05105750d379b416918566f1b727e5e91d8
Chris Mytton <self@hecticjeff.net>
2012-04-06 12:42:19 +0100

    Update tests to use bash

--- test/shoreman_test.sh
+++ test/shoreman_test.sh
@@ -1,16 +1,16 @@
 describe "Shoreman"
 
 it_displays_usage() {
-  usage=$(sh ./shoreman.sh --help | head -n1)
+  usage=$(bash ./shoreman.sh --help | head -n1)
   test "$usage" = "Usage: shoreman [<procfile>]"
 }
 
 it_runs_simple_processes() {
-  output=$(sh ./shoreman.sh 'test/fixtures/simple_procfile' | head -n1)
+  output=$(bash ./shoreman.sh 'test/fixtures/simple_procfile' | head -n1)
   test "$output" = "Hello"
 }
 
 it_passes_environment_variables_to_processes() {
-  output=$(FOO=bar sh ./shoreman.sh 'test/fixtures/environment_procfile' | head -n1)
+  output=$(FOO=bar bash ./shoreman.sh 'test/fixtures/environment_procfile' | head -n1)
   test "$output" = "FOO = bar"
 }
