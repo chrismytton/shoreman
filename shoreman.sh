c668de0c3e01fd3d44d61153394b206738fd7459
David Pelaez <david@vlipco.co>
2014-05-07 18:54:00 -0500

    Multiple improvements in shoreman

--- shoreman.sh
+++ shoreman.sh
@@ -1,59 +1,62 @@
 #!/bin/bash
+set -eo pipefail
 
-# [shoreman](https://github.com/hecticjeff/shoreman) is an
-# implementation of the **Procfile** format. Inspired by the original
-# [foreman](http://ddollar.github.com/foreman/) tool for ruby, as
-# well as [norman](https://github.com/josh/norman) for node.js.
+# Temporary empty dir to created named pipes (fifos)
+# that will be needed to keep track of the created process PIDs
+# while also formatting their output at the same time.
+temp_dir=`mktemp -d .tmp.XXXXXXXXXX`
 
-# Make sure that any errors cause the script to exit immediately.
-set -e
-
-# ## Usage
-
-# Usage message that is displayed when `--help` is given as an argument.
-usage() {
-  echo "Usage: shoreman [<procfile>]"
-  echo "Run Procfiles using shell."
-  echo
-  echo "The shoreman script reads commands from <procfile> and starts up the"
-  echo "processes that it describes."
-}
-
-# If the --help option is given, show the usage message and exit.
-expr -- "$*" : ".*--help" >/dev/null && {
-  usage
-  exit 0
-}
-
-# ## Logging
-
-# For logging we want to prefix each entry with the current time, as well
-# as the process name. This takes one argument, the name of the process, and
-# then reads data from stdin, formats it, and sends it to stdout.
+# This function formats all text coming from STDIN to look like:
+# process_name | output
+# ensuring a fixed distance of 12 chars until the separating pipe
+# the result is that it's clear what's coming from where.
+# the usage is: command_to_format | log process_name
 log() {
-  while read data
-  do
-    __TAB_CHARACTER=$'\t'
-    echo "$(date +"%H:%M:%S") ${1}${__TAB_CHARACTER}| $data"
-  done
+  awk_cmd="{ printf \"%-12s | %s\\n\", \"$1\", \$1}"
+  cat - | awk -F'|' "$awk_cmd"
 }
 
-# ## Running commands
-
-# When a process is started, we want to keep track of its pid so we can
-# `kill` it when the parent process receives a signal, and so we can `wait`
-# for it to finish before exiting the parent process.
-store_pid() {
-  pids=("${pids[@]}" "$1")
+# Associative data storage use to link a process PID with
+# the name it was given in the Procfile
+hput () {
+  eval hash"$1"='$2'
 }
 
-# This starts a command asynchronously and stores its pid in a list for use
-# later on in the script.
+hget () {
+  eval echo '${hash'"$1"'#hash}'
+}
+
+# Convenient function to scope shoreman's own output
+info() {
+  echo "$1" | log "shoreman"
+}
+
+# When a process with a name and a command is read from the
+# Profile. this function launches the command, stores it's pid
+# informs that PID to the user and then passed the command output
+# through the log function for pretty formatting.
 start_command() {
-  bash -c "$1" &
+  name=$2
+  cmd=$1
+
+  # Named pipes are used to know the launched command PID
+  # while keeping the posibility of redirecting it's output
+  cmd_fifo=$temp_dir/$name
+  mkfifo $cmd_fifo
+
+  # We send the executed command output to 
+  #bash -lc "$cmd &> $cmd_fifo" &
+  ($cmd &> $cmd_fifo) &
   pid="$!"
-  store_pid "$pid"
+  pids=("${pids[@]}" "$pid")
+
+  log $name < $cmd_fifo &
+  # Associate pid with name for better error messages
+  hput $pid $name
+  info "$name started with pid=$pid"
 }
+
+info "shoreman started with pid=$$"
 
 # ## Reading the .env file
 
@@ -61,39 +64,79 @@ start_command() {
 # Only lines containing an equal sign are read, which means you can add comments.
 # Preferably shell-style comments so that your editor print them like shell scripts.
 
-ENV_FILE=${2:-'.env'}
-if [ -f $ENV_FILE ]; then
+env_file=${2:-'.env'}
+if [ -f $env_file ]; then
   while read line || [ -n "$line" ]; do
     if [[ "$line" != \#* && "$line" == *=* ]]; then
       eval "export $line"
     fi
-  done < "$ENV_FILE"
+  done < "$env_file"
 fi
 
 # ## Reading the Procfile
 
 # The Procfile needs to be parsed to extract the process names and commands.
 # The file is given on stdin, see the `<` at the end of this while loop.
-PROCFILE=${1:-'Procfile'}
+procfile=${1:-'Procfile'}
 while read line || [ -n "$line" ]; do
   name=${line%%:*}
   command=${line#*: }
-  start_command "$command"
-  echo "'${command}' started with pid ${pid}" | log "${name}.1"
-done < "$PROCFILE"
+  start_command "$command" $name
+done < "$procfile"
 
-# ## Cleanup
+# ## Process management
 
-# When a `SIGINT`, `SIGTERM` or `EXIT` is received, this action is run, killing the
-# child processes. The sleep stops STDOUT from pouring over the prompt, it
-# should probably go at some point.
-onexit() {
-  echo SIGINT received
-  echo sending SIGTERM to all processes
-  kill ${pids[*]} &>/dev/null
-  sleep 1
+# This sends the signal passed as first argument to all alive processes
+# in the $pids array, ignoring errors.  
+# It prints log messages if the second parameter is 1.
+send_signal() {
+  for pid in "${pids[@]}"; do
+    # Only run if the process exists
+    if kill -0 $pid &> /dev/null; then
+      if [[ "$2" == "1" ]]; then
+        info "Sending $1 to `hget $pid`"
+      fi
+      # Ignore errors
+      kill -s $1 $pid &> /dev/null || :
+    fi
+  done
 }
-trap onexit SIGINT SIGTERM EXIT
 
-# Wait for the children to finish executing before exiting.
-wait
+# When a `SIGINT` or `SIGTERM` is received, this action is run, signaling the
+# child processes with SIGTERM to gracefully shutdown within 3 seconds.
+# After that waitime, any remaining children are all killed with SIGKILL.
+# Shoreman does its best to avoid any process from outliving it.
+onexit() {
+  # An explanation for the exit is passed as first param
+  info "$1"
+  info "Terminating all processes"
+
+  # the 0 means don't print messages
+  send_signal SIGTERM 1
+  sleep 3
+
+  # we disown all jobs to prevent error messages
+  # when we try to kill them
+  disown -a
+
+  # the 1 causes a message to be printed for each
+  # misbehaving process that didn't exit gracefully
+  send_signal SIGKILL 1
+
+  exit 0
+}
+
+trap 'onexit "SIGINT received."' SIGINT
+trap 'onexit "SIGTERM received."' SIGTERM
+
+# We mind our manners and remove our temporary files upon exit
+trap "rm -rf $temp_dir" EXIT
+
+while true; do
+  for pid in "${pids[@]}"; do
+    if ! kill -0 $pid &> /dev/null; then
+      onexit "Error: `hget $pid` is no longer running"
+    fi
+  done
+  sleep 0.5
+done
