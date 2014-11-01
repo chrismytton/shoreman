6b41703c92445ce0cd3cc3bcf1cd75996427125e
Chris Mytton <self@hecticjeff.net>
2014-11-01 14:17:00 +0000

    Refactor code to be in functions

--- shoreman.sh
+++ shoreman.sh
@@ -7,6 +7,7 @@
 
 # Make sure that any errors cause the script to exit immediately.
 set -e
+[ "$TRACE" ] && set -x
 
 # ## Usage
 
@@ -19,29 +20,24 @@ usage() {
   echo "processes that it describes."
 }
 
-# If the --help option is given, show the usage message and exit.
-expr -- "$*" : ".*--help" >/dev/null && {
-  usage
-  exit 0
-}
-
 # ## Logging
 
 # For logging we want to prefix each entry with the current time, as well
 # as the process name. This takes one argument, the name of the process, and
 # then reads data from stdin, formats it, and sends it to stdout.
-log() { (
+log() {
+  local pid="$!"
   # Bash colors start from 31 up to 38. Instead of a hash set and storing a
   # bunch of variables, we will simply calculate what color the process will get
   # base on its PID
-  color=$((31 + ($! % 7)))
+  local color=$((31 + (pid % 7)))
 
   while read -r data
   do
     printf "\033[1;%sm%s %s\033[0m" "$color" "$(date +"%H:%M:%S")" "$1"
     printf "\t| %s\n" "$data"
   done
-) }
+}
 
 # ## Running commands
 
@@ -61,28 +57,31 @@ start_command() {
 
 # ## Reading the .env file
 
-# Set a default port before loading the .env file
-export PORT=${PORT:-5000}
-
 # The .env file needs to be a list of assignments like in a shell script.
 # Shell-style comments are permitted.
+load_env_file() {
+  # Set a default port before loading the .env file
+  export PORT=${PORT:-5000}
 
-ENV_FILE=${2:-'.env'}
-if [ -f "$ENV_FILE" ]; then
-  export $(grep "^[^#]*=.*" "$ENV_FILE" | xargs)
-fi
+  ENV_FILE=${1:-'.env'}
+  if [ -f "$ENV_FILE" ]; then
+    export $(grep "^[^#]*=.*" "$ENV_FILE" | xargs)
+  fi
+}
 
 # ## Reading the Procfile
 
 # The Procfile needs to be parsed to extract the process names and commands.
 # The file is given on stdin, see the `<` at the end of this while loop.
-PROCFILE=${1:-'Procfile'}
-while read line || [ -n "$line" ]; do
-  name=${line%%:*}
-  command=${line#*:[[:space:]]}
-  start_command "$command" "${name}"
-  echo "'${command}' started with pid $!" | log "${name}"
-done < "$PROCFILE"
+run_procfile() {
+  local procfile=${1:-'Procfile'}
+  while read line || [ -n "$line" ]; do
+    local name="${line%%:*}"
+    local command="${line#*:[[:space:]]}"
+    start_command "$command" "${name}"
+    echo "'${command}' started with pid $!" | log "${name}"
+  done < "$procfile"
+}
 
 # ## Cleanup
 
@@ -95,7 +94,24 @@ onexit() {
   kill $pids
   sleep 1
 }
-trap onexit INT TERM EXIT
 
-# Wait for the children to finish executing before exiting.
-wait
+main() {
+  local procfile="$1"
+  local env_file="$2"
+
+  # If the --help option is given, show the usage message and exit.
+  expr -- "$*" : ".*--help" >/dev/null && {
+    usage
+    exit 0
+  }
+
+  load_env_file "$env_file"
+  run_procfile "$procfile"
+
+  trap onexit INT TERM
+
+  # Wait for the children to finish executing before exiting.
+  wait $pids
+}
+
+main "$@"
